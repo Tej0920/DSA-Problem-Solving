82dfac648682ccb0cb18eaa050522b048c55b929
Tej0920 <48469387+Tej0920@users.noreply.github.com>
2021-08-12 20:35:47 +0530

    Create K_Largest_Elements.cpp

---
+++ InterviewBit/Heaps-And-Maps/K_Largest_Elements.cpp
@@ -0,0 +1,21 @@
+vector<int> Solution::solve(vector<int> &A, int B) {
+    priority_queue<int,vector<int>, greater<int>> pq;
+    
+    for(int i=0;i<A.size();i++){
+        pq.push(A[i]);
+        if(pq.size()>B){
+            pq.pop();
+        }
+        
+    }
+    
+    vector<int> output;
+    
+    while(!pq.empty()){
+        output.push_back(pq.top());
+        pq.pop();
+    }
+    
+    return output;
+    
+}
